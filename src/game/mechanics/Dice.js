45edb0afce410b310f0fdf64658e69eaa7f3310a
Anthony <anthony.jackson.002@gmail.com>
2021-05-23 16:31:18 -0400

    Dice.js now rolls dice and can create starting stats

--- src/game/mechanics/Dice.js
+++ src/game/mechanics/Dice.js
@@ -1,161 +1,127 @@
-export class Dice{
-	roll = 0;// Holder for dice
+export default class Dice{
+	constructor() { }
 
-    getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-    }
-
+	getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+	}
+	
 	fourSided() {
-		roll = getRandomInt(1, 4);
-		return roll;
+		return this.getRandomInt(1, 4);
 	}
 
 	sixSided() {
-		roll = getRandomInt(1, 6);
-		return roll;
+		return this.getRandomInt(1, 6);
 	}
 
 	eightSided() {
-		roll = getRandomInt(1, 8);
-		return roll;
+		return this.getRandomInt(1, 8);
 	}
 
 	tenSided() {
-		roll = getRandomInt(1, 10);;
-		return roll;
+		return this.getRandomInt(1, 10);
 	}
 
 	twelveSided() {
-		roll = getRandomInt(1, 12);
-		return roll;
+		return this.getRandomInt(1, 12);
 	}
 
 	twentySided() {
-		roll = 1 + getRandomInt(1, 20);
-		return roll;
+		return this.getRandomInt(1, 20);
 	}
 
 	percentage() {
-		roll = getRandomInt(0, 9) * 10 + dice.nextInt(0, 10);
-		return roll;
+		return this.getRandomInt(0, 9) * 10 + this.getRandomInt(0, 10);
 	}
 
 	multipleRolls(rolls, sided) {
-		diceRolls = [];// Holds dice rolls
+		let diceRolls = [];// Holds dice rolls
 
-		diceRolls.clear();
-		for (i = 0; i < rolls; i++) {
+		for (let i = 0; i < rolls; i++) {
 			switch (sided) {
 			case 4:
-				diceRolls.push(fourSided());
+				diceRolls.push(this.fourSided());
 				break;
 			case 6:
-				diceRolls.push(sixSided());
+				diceRolls.push(this.sixSided());
 				break;
 			case 10:
-				diceRolls.push(tenSided());
+				diceRolls.push(this.tenSided());
 				break;
 			case 12:
-				diceRolls.push(twelveSided());
+				diceRolls.push(this.twelveSided());
 				break;
 			case 20:
-				diceRolls.push(twentySided());
+				diceRolls.push(this.twentySided());
 				break;
 			case 100:
-				diceRolls.push(percentage());
+				diceRolls.push(this.percentage());
 				break;
 			}
 		}
 		return diceRolls;
 	}
 
-	combineRolls(rolls) {
-		combinedRolls = 0;
+	startingStats() {
+		let rollHolder = this.rollStats();
+	
+		rollHolder = this.removeLowestState(rollHolder);
+		
+		return this.calcStats(rollHolder);
+	}
 
-		for (roll of rolls) {
+	rollStats(){
+		let rollHolder = [];
+
+		for (let i = 0; i < 6; i++) {
+			rollHolder.push([]);
+			for (let r = 0; r < 4; r++) {
+				rollHolder[i][r] = this.sixSided();
+			}
+		}
+
+		return rollHolder;
+	}
+
+	removeLowestState(rollHolder){
+		for (let i = 0; i < 6; i++) {
+			if (rollHolder[i][0] < rollHolder[i][1] || rollHolder[i][0] < rollHolder[i][2]
+					|| rollHolder[i][0] < rollHolder[i][3]) {
+				rollHolder[i][0] = 0;
+			} else if (rollHolder[i][1] < rollHolder[i][0] || rollHolder[i][1] < rollHolder[i][2]
+					|| rollHolder[i][1] < rollHolder[i][3]) {
+				rollHolder[i][1] = 0;
+			} else if (rollHolder[i][2] < rollHolder[i][0] || rollHolder[i][2] < rollHolder[i][1]
+					|| rollHolder[i][2] < rollHolder[i][3]) {
+				rollHolder[i][2] = 0;
+			} else if (rollHolder[i][3] < rollHolder[i][0] || rollHolder[i][3] < rollHolder[i][1]
+					|| rollHolder[i][3] < rollHolder[i][2]) {
+				rollHolder[i][3] = 0;
+			}
+		}
+
+		return rollHolder;
+	}
+
+	combineRolls(rolls) {
+		let combinedRolls = 0;
+
+		for (let roll of rolls) {
 			combinedRolls += roll;
 		}
 
 		return combinedRolls;
 	}
 
-	startingStats() {
-		stats = new int[6];
-		rollHolder = new int[6][4];
-	    statAmount = 0; 
+	calcStats(rollHolder){
+		let stats = [];
 
-		for (i = 0; i < 6; i++) {
-			for (r = 0; r < 4; r++) {
-				rollHolder[i][r] = 1 + dice.nextInt(6);
-			}
-		}
-
-		for (i = 0; i < 6; i++) {
-			if (rollHolder[i][0] < rollHolder[i][1] || rollHolder[i][0] < rollHolder[i][2]
-					|| rollHolder[i][0] < rollHolder[i][3]) {
-				rollHolder[i][0] = 0;
-			} else if (rollHolder[i][1] < rollHolder[i][0] || rollHolder[i][1] < rollHolder[i][2]
-					|| rollHolder[i][1] < rollHolder[i][3]) {
-				rollHolder[i][1] = 0;
-			} else if (rollHolder[i][2] < rollHolder[i][0] || rollHolder[i][2] < rollHolder[i][1]
-					|| rollHolder[i][2] < rollHolder[i][3]) {
-				rollHolder[i][2] = 0;
-			} else if (rollHolder[i][3] < rollHolder[i][0] || rollHolder[i][3] < rollHolder[i][1]
-					|| rollHolder[i][3] < rollHolder[i][2]) {
-				rollHolder[i][3] = 0;
-			}
-		}
-
-		for (i = 0; i < 6; i++) {
-			statAmount = 0;
-			for (r = 0; r < 4; r++) {
-				statAmount += rollHolder[i][r];
-			}
-			stats[i] = statAmount;
-		}
-		return stats;
-	}
-
-	randomStats() {
-		stats = [];
-	    rollHolder = [];
-		statAmount = 0;
-
-		for (i = 0; i < 6; i++) {
-            rollHolder.push([])
-			for (r = 0; r < 4; r++) {
-				rollHolder[i][r] = sixSided();
-			}
-		}
-
-		for (i = 0; i < 6; i++) {
-			if (rollHolder[i][0] < rollHolder[i][1] || rollHolder[i][0] < rollHolder[i][2]
-					|| rollHolder[i][0] < rollHolder[i][3]) {
-				rollHolder[i][0] = 0;
-			} else if (rollHolder[i][1] < rollHolder[i][0] || rollHolder[i][1] < rollHolder[i][2]
-					|| rollHolder[i][1] < rollHolder[i][3]) {
-				rollHolder[i][1] = 0;
-			} else if (rollHolder[i][2] < rollHolder[i][0] || rollHolder[i][2] < rollHolder[i][1]
-					|| rollHolder[i][2] < rollHolder[i][3]) {
-				rollHolder[i][2] = 0;
-			} else if (rollHolder[i][3] < rollHolder[i][0] || rollHolder[i][3] < rollHolder[i][1]
-					|| rollHolder[i][3] < rollHolder[i][2]) {
-				rollHolder[i][3] = 0;
-			}
-		}
-
-		for (i = 0; i < 6; i++) {
-			statAmount = 0;
-			for (r = 0; r < 4; r++) {
-				statAmount += rollHolder[i][r];
-			}
-			stats.push(statAmount);
+		for (let i = 0; i < 6; i++) {
+			stats.push(this.combineRolls(rollHolder[i]));
 		}
 
 		return stats;
 	}
-
-
 }
+
